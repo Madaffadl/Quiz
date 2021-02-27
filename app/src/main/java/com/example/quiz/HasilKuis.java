b39c8f164f8301665b11ddfa7ada403300df7310
Madaffadl26 <@m.daffafadhil26@gmail.com>
2021-02-27 22:07:28 +0700

    daffa

---
+++ app/src/main/java/com/example/quiz/HasilKuis.java
@@ -0,0 +1,32 @@
+package com.example.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class HasilKuis extends AppCompatActivity {
+
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil_kuis);
+
+        TextView hasil = (TextView) findViewById(R.id.hasil);
+        TextView nilai = (TextView) findViewById(R.id.nilai);
+
+        hasil.setText("Jawaban Benar :" + MainActivity.benar + "\nJawaban Salah :" + MainActivity.salah);
+        nilai.setText("" + MainActivity.hasil);
+    }
+
+    public void ulangi(View view) {
+        finish();
+        Intent a = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(a);
+    }
+}
